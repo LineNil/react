a76d51a28852763b16617bb52734dbae42de0852
Line Nilsen <lineforslund@hotmail.com>
2024-03-04 14:29:06 +0100

    components

---
+++ ecom-shop/src/components/App/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../Layout";
+
+
+function App(){
+  return(
+    <div>
+      <Layout>
+       <p>Innhold</p> 
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
---
+++ ecom-shop/src/components/Nav/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Nav(){
+  return (
+    <nav>
+      <ul>
+        <li>hjem</li>
+        <li>hjem</li>
+        <li>hjem</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
